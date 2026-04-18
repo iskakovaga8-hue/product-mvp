e57f4b61dc98f98c7050f4d30085fde97de75972
Your Name <you@example.com>
2026-04-18 16:14:01 +0300

    Try transpilePackages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbo: {
-    resolveExtensions: [".tsx", ".ts", ".jsx", ".js"],
-  },
+  transpilePackages: ["@"],
 };
 
 export default nextConfig;
